42c6e987742fc461bf8e7f163a30055af4f3fb1e
ostmany <exau.deleon@gmail.com>
2015-06-11 01:12:28 -0600

    cambios

---
+++ SysVYC/SysVYC/Controllers/IndexController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SysVYC.Models;
+
+namespace SysVYC.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(USUARIO u)
+        {
+            if(ModelState.IsValid)
+            {
+                //byte[] decryted = Convert.FromBase64String();
+                using(SVYC db = new SVYC())
+                {
+                    var v = db.USUARIO.Where(a => a.USERNAME.Equals(u.USERNAME) && a.PASSWORD.Equals(u.PASSWORD)).FirstOrDefault();
+                    if(v != null)
+                    {
+                        Session["IDUSUARIO"] = v.IDUSUARIO.ToString();
+                        Session["IDTIPOUSUARIO"] = v.IDTIPOUSUARIO.ToString();
+                        if(Session["IDTIPOUSUARIO"].ToString().Equals("1"))
+                            return RedirectToAction("Principal");
+                        else
+                            return RedirectToAction("Negocio");
+                    }
+                }
+
+            }
+            return View(u);
+        }
+
+        public ActionResult Principal()
+        {
+            if (Session["IDUSUARIO"] != null)
+                return View();
+            else
+                return RedirectToAction("Index");   
+        }
+
+        public ActionResult Negocio()
+        {
+            if (Session["IDUSUARIO"] != null)
+                return View();
+            else
+                return RedirectToAction("Index");
+        }
+
+        public ActionResult CerrarSession()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
---
+++ SysVYC/SysVYC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SysVYC.Startup))]
+namespace SysVYC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
